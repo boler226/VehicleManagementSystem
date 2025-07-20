b812cfad0a150c43514b4e0f387011ac5c62de7e
Illya <illamatviuk@gmail.com>
2025-07-20 13:23:45 +0300

    Added Transpost Controller.

---
+++ VehicleManagementSystem.API/Controllers/TransportController.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using VehicleManagementSystem.Application.Commands.AddTransport;
+using VehicleManagementSystem.Application.Queries.GetAllTransports;
+
+namespace VehicleManagementSystem.API.Controllers {
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class TransportController(IMediator mediator) : ControllerBase {
+        [HttpGet]
+        public async Task<IActionResult> GetAll() =>
+            Ok(await mediator.Send(new GetAllTransportsQuery()));
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddTransportCommand command) =>
+            Ok(await mediator.Send(command));
+    }
+}
