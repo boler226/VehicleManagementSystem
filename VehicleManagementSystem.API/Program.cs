dddaa7cf2e82daa993d6bb5a4be81e465fb48a57
Illya <illamatviuk@gmail.com>
2025-07-20 13:24:32 +0300

    Added Program.cs with DI and settings.json.

--- VehicleManagementSystem.API/Program.cs
+++ VehicleManagementSystem.API/Program.cs
@@ -1,8 +1,24 @@
+using VehicleManagementSystem.Application.Commands.AddTransport;
+using VehicleManagementSystem.Application.Mappings;
+using VehicleManagementSystem.Domain.Interfaces;
+using VehicleManagementSystem.Infrastructure.DbContext;
+using VehicleManagementSystem.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<MongoDbContext>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<ITransportRepository, TransportRepository>();
+
+
+builder.Services.AddAutoMapper(typeof(TransportMappingProfile).Assembly);
+builder.Services.AddMediatR(cfg =>
+    cfg.RegisterServicesFromAssembly(typeof(AddTransportCommand).Assembly));
+
 
 var app = builder.Build();
 
