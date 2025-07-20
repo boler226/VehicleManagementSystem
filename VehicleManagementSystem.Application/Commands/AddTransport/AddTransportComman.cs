143355b9c06c1b1a28969f3282f91d059daa95e1
Illya <illamatviuk@gmail.com>
2025-07-20 13:22:27 +0300

    Added AddTransport Command and Handler.

---
+++ VehicleManagementSystem.Application/Commands/AddTransport/AddTransportComman.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using VehicleManagementSystem.Application.DTOs.Transport;
+using VehicleManagementSystem.Domain.Enums;
+
+namespace VehicleManagementSystem.Application.Commands.AddTransport {
+    public record AddTransportCommand(
+        string LicensePlate,
+        string Brand,
+        string Model,
+        TransportEnum Type,
+        int? Capacity,
+        double? LoadCapacity
+    ) : IRequest<Guid>;
+}
---
+++ VehicleManagementSystem.Application/Commands/AddTransport/AddTransportCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using VehicleManagementSystem.Domain.Entities;
+using VehicleManagementSystem.Domain.Interfaces;
+
+namespace VehicleManagementSystem.Application.Commands.AddTransport
+{
+    public class AddTransportCommandHandler(
+        ITransportRepository repository
+        ) : IRequestHandler<AddTransportCommand, Guid> {
+        public async Task<Guid> Handle(AddTransportCommand request, CancellationToken cancellationToken) {
+            var entity = new TransportEntity {
+                Id = Guid.NewGuid(),
+                LicensePlate = request.LicensePlate,
+                Brand = request.Brand,
+                Model = request.Model,
+                Type = request.Type,
+                Capacity = request.Capacity,
+                LoadCapacity = request.LoadCapacity,
+                IsWrittenOff = false
+            };
+
+            await repository.AddAsync(entity, cancellationToken);
+            return entity.Id;
+        }
+    }
+}
