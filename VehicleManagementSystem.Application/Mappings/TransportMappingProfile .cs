1589693552d1497f2340ed3b4c806ee794204542
Illya <illamatviuk@gmail.com>
2025-07-20 13:21:49 +0300

    Added Transport mappings.

---
+++ VehicleManagementSystem.Application/Mappings/TransportMappingProfile .cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using VehicleManagementSystem.Application.DTOs;
+using VehicleManagementSystem.Application.DTOs.RouteAssignment;
+using VehicleManagementSystem.Application.DTOs.Transport;
+using VehicleManagementSystem.Domain.Entities;
+
+namespace VehicleManagementSystem.Application.Mappings
+{
+    public class TransportMappingProfile : Profile {
+        public TransportMappingProfile() {
+            CreateMap<TransportEntity, TransportWithRoutesDto>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Routes, opt => opt.MapFrom(src => src.RouteAssignments));
+
+            CreateMap<RouteAssignmentEntity, RouteAssignmentDto>();
+
+            CreateMap<TransportEntity, TransportDto>();
+            CreateMap<TransportEntity, TransportBasicDto>();
+
+            CreateMap<RouteEntity, RouteDto>();
+
+            CreateMap<RouteAssignmentEntity, RouteAssignmentDto>();
+            CreateMap<RouteAssignmentEntity, RouteAssignmentSimpleDto>();
+        }
+    }
+}
