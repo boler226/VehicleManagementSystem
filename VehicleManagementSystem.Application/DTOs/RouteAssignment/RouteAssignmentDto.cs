1291fe2883d8c0770fee185f15961d62d2798f23
Illya <illamatviuk@gmail.com>
2025-07-20 13:21:00 +0300

    Added RouteAssingmentDto.

---
+++ VehicleManagementSystem.Application/DTOs/RouteAssignment/RouteAssignmentDto.cs
@@ -0,0 +1,14 @@
+﻿namespace VehicleManagementSystem.Application.DTOs.RouteAssignment
+{
+    public class RouteAssignmentDto
+    {
+        public Guid Id { get; set; }
+        public Guid TransportId { get; set; }
+        public TransportBasicDto Transport { get; set; } = null!;
+        public Guid RouteId { get; set; }
+        public RouteDto Route { get; set; } = null!;
+
+        public DateTime Date { get; set; }
+        public int PassengersCarried { get; set; }
+    }
+}
---
+++ VehicleManagementSystem.Application/DTOs/RouteAssignment/TransportBasicDto.cs
@@ -0,0 +1,10 @@
+﻿using VehicleManagementSystem.Domain.Enums;
+
+namespace VehicleManagementSystem.Application.DTOs.RouteAssignment {
+    public class TransportBasicDto {
+        public Guid Id { get; set; }
+        public string LicensePlate { get; set; } = null!;
+        public TransportEnum Type { get; set; }
+    }
+
+}
