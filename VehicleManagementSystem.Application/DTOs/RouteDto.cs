8460fb6dac03f1101b87ec5317bef8d09ea0d9e7
Illya <illamatviuk@gmail.com>
2025-07-20 13:21:30 +0300

    Added Route Dto.

---
+++ VehicleManagementSystem.Application/DTOs/RouteDto.cs
@@ -0,0 +1,7 @@
+﻿namespace VehicleManagementSystem.Application.DTOs {
+    public class RouteDto {
+        public Guid Id { get; set; }
+        public string RouterNumber { get; set; } = null!;
+        public string Description { get; set; } = null!;
+    }
+}
