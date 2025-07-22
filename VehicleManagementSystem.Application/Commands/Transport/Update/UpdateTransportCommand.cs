903c7b91becb7f2cbcb42bb5cb1fd9cc8b84028d
Illya <illamatviuk@gmail.com>
2025-07-22 19:58:09 +0300

    UpdateTransportCommand and Handler.

---
+++ VehicleManagementSystem.Application/Commands/Transport/Update/UpdateTransportCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using VehicleManagementSystem.Domain.Enums;
+
+namespace VehicleManagementSystem.Application.Commands.Transport.Update {
+    public record UpdateTransportCommand(
+        Guid Id,
+        string? LicensePlate,
+        TransportEnum? Type,
+        int? Capacity,
+        double? LoadCapacity
+    ) : IRequest<Unit>;
+}
---
+++ VehicleManagementSystem.Application/Commands/Transport/Update/UpdateTransportCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using VehicleManagementSystem.Domain.Interfaces;
+
+namespace VehicleManagementSystem.Application.Commands.Transport.Update {
+    public class UpdateTransportCommandHandler(
+        IUnitOfWork unitOfWork
+        ) : IRequestHandler<UpdateTransportCommand, Unit> {
+        public async Task<Unit> Handle(UpdateTransportCommand request, CancellationToken cancellationToken) {
+            var transport = await unitOfWork.Transports.GetByIdAsync(request.Id, cancellationToken);
+
+            if (transport is null)
+                throw new Exception("Transport not found");
+
+            if (request.LicensePlate is not null)
+                transport.LicensePlate = request.LicensePlate;
+
+            if (request.Type.HasValue)
+                transport.Type = request.Type.Value;
+
+            if (request.Capacity is not null)
+                transport.Capacity = request.Capacity;
+
+            if (request.LoadCapacity is not null)
+                transport.LoadCapacity = request.LoadCapacity;
+
+            await unitOfWork.Transports.UpdateAsync(transport, cancellationToken);
+            return Unit.Value;
+        }
+    }
+}
