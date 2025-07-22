b2024b1381a39463d6b116c7b46fe8e4d869c8e8
Illya <illamatviuk@gmail.com>
2025-07-22 20:00:12 +0300

    Update TransportController, added Update and Delete.

--- VehicleManagementSystem.API/Controllers/TransportController.cs
+++ VehicleManagementSystem.API/Controllers/TransportController.cs
@@ -1,9 +1,12 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using VehicleManagementSystem.Application.Commands.AddTransport;
+using VehicleManagementSystem.Application.Commands.Transport.AddTransport;
+using VehicleManagementSystem.Application.Commands.Transport.DeleteTransport;
+using VehicleManagementSystem.Application.Commands.Transport.Update;
 using VehicleManagementSystem.Application.Queries.GetAllTransports;
 
-namespace VehicleManagementSystem.API.Controllers {
+namespace VehicleManagementSystem.API.Controllers
+{
     [ApiController]
     [Route("api/[controller]/[action]")]
     public class TransportController(IMediator mediator) : ControllerBase {
@@ -14,5 +17,13 @@ namespace VehicleManagementSystem.API.Controllers {
         [HttpPost]
         public async Task<IActionResult> Add(AddTransportCommand command) =>
             Ok(await mediator.Send(command));
-    }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(UpdateTransportCommand command) =>
+            Ok(await mediator.Send(command));
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id) =>
+            Ok(await mediator.Send(new DeleteTransportCommand(id)));
+    } 
 }
