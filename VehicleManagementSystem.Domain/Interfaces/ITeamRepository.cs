b8f360dbcdd03accd4e688dbb2726ac39e16dcf2
Illya <illamatviuk@gmail.com>
2025-07-22 19:55:30 +0300

    Added TeamRepository.

---
+++ VehicleManagementSystem.Domain/Interfaces/ITeamRepository.cs
@@ -0,0 +1,7 @@
+﻿using VehicleManagementSystem.Domain.Entities;
+
+namespace VehicleManagementSystem.Domain.Interfaces {
+    public interface ITeamRepository {
+        Task<TeamEntity?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+    }
+}
---
+++ VehicleManagementSystem.Infrastructure/Repositories/TeamRepository.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Driver;
+using VehicleManagementSystem.Domain.Entities;
+using VehicleManagementSystem.Domain.Interfaces;
+using VehicleManagementSystem.Infrastructure.DbContext;
+
+namespace VehicleManagementSystem.Infrastructure.Repositories {
+    public class TeamRepository : ITeamRepository {
+        private readonly IMongoCollection<TeamEntity> _collection;
+
+        public TeamRepository(MongoDbContext context) {
+            _collection = context.GetCollection<TeamEntity>("Teams");
+        }
+
+        public async Task<TeamEntity?> GetByIdAsync(Guid id, CancellationToken cancellationToken) =>
+            await _collection.Find(t => t.Id == id).FirstOrDefaultAsync(cancellationToken);
+    }
+}
