80283eaa40127af75d4bdb98077c1d7eceef3c56
Illya <illamatviuk@gmail.com>
2025-07-22 19:58:39 +0300

    Added AddDriverCommand and Handler.

---
+++ VehicleManagementSystem.Application/Commands/Driver/Add/AddDriverCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace VehicleManagementSystem.Application.Commands.Driver.Add {
+    public record AddDriverCommand(string FullName, string LicenseNumber, Guid TeamId) : IRequest<Guid>;
+}
---
+++ VehicleManagementSystem.Application/Commands/Driver/Add/AddDriverCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using VehicleManagementSystem.Domain.Entities;
+using VehicleManagementSystem.Domain.Interfaces;
+
+namespace VehicleManagementSystem.Application.Commands.Driver.Add {
+    public class AddDriverCommandHandler(
+        IUnitOfWork unitOfWork
+        ) : IRequestHandler<AddDriverCommand, Guid> {
+        public async Task<Guid> Handle(AddDriverCommand request, CancellationToken cancellationToken) {
+            var team = await unitOfWork.Teams.GetByIdAsync(request.TeamId, cancellationToken);
+
+            if (team is null)
+                throw new Exception("Team not found");
+
+            var entity = new DriverEntity {
+                Id = Guid.NewGuid(),
+                FullName = request.FullName,
+                LicenseNumber = request.LicenseNumber,
+                TeamId = team.Id,
+                Team = team
+            };
+
+            await unitOfWork.Drivers.AddAsync(entity, cancellationToken);
+            return entity.Id;
+        }
+    }
+}
