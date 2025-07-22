74d86fae9b35a95418bd545f47a117106c3fa7af
Illya <illamatviuk@gmail.com>
2025-07-22 19:56:51 +0300

    Added Driver dto.

---
+++ VehicleManagementSystem.Application/DTOs/Driver/DriverDto.cs
@@ -0,0 +1,9 @@
+﻿namespace VehicleManagementSystem.Application.DTOs.Driver {
+    public class DriverDto {
+        public Guid Id { get; set; }
+        public string FullName { get; set; } = null!;
+        public string LicenseNumber { get; set; } = null!;
+        public Guid TeamId { get; set; }
+    }
+
+}
