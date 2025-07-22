c7717295687a3190fb4a2bca984414329ac969b0
Illya <illamatviuk@gmail.com>
2025-07-22 19:56:00 +0300

    Added UnitOfWork (without commit).

---
+++ VehicleManagementSystem.Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,7 @@
+﻿namespace VehicleManagementSystem.Domain.Interfaces {
+    public interface IUnitOfWork {
+        IDriverRepository Drivers { get; }
+        ITransportRepository Transports { get; } 
+        ITeamRepository Teams { get; }
+    }
+}
---
+++ VehicleManagementSystem.Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using VehicleManagementSystem.Domain.Interfaces;
+
+namespace VehicleManagementSystem.Infrastructure.UnitOfWork {
+    public class UnitOfWork(
+        IDriverRepository driverRepository,
+        ITransportRepository transportRepository,
+        ITeamRepository teamRepository
+        ) : IUnitOfWork {
+        public IDriverRepository Drivers => driverRepository;
+        public ITransportRepository Transports => transportRepository;
+        public ITeamRepository Teams => teamRepository;
+    }
+}
