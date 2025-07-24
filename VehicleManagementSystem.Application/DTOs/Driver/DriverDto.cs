87c6b37c8297c93337146f7d48ce15eec7e8f7f0
Illya <illamatviuk@gmail.com>
2025-07-24 15:17:45 +0300

    Update DriverDto.

--- VehicleManagementSystem.Application/DTOs/Driver/DriverDto.cs
+++ VehicleManagementSystem.Application/DTOs/Driver/DriverDto.cs
@@ -4,6 +4,9 @@
         public string FullName { get; set; } = null!;
         public string LicenseNumber { get; set; } = null!;
         public Guid TeamId { get; set; }
+
+        public string? TeamName { get; set; }
+        public int VehicleCount { get; set; }
     }
 
 }
