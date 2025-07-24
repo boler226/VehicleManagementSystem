926bae337a0ee4f63bbbbd1133db9ab479ab47c5
Illya <illamatviuk@gmail.com>
2025-07-24 15:20:57 +0300

    Changed DeleteTransportCommandHandler, added remove DriverTransportByTransportId.

--- VehicleManagementSystem.Application/Commands/Transport/Delete/DeleteTransportCommandHandler.cs
+++ VehicleManagementSystem.Application/Commands/Transport/Delete/DeleteTransportCommandHandler.cs
@@ -1,20 +1,21 @@
 ﻿using MediatR;
 using VehicleManagementSystem.Domain.Interfaces;
+using VehicleManagementSystem.Domain.Interfaces.Repositories;
 
 namespace VehicleManagementSystem.Application.Commands.Transport.DeleteTransport
 {
     public class DeleteTransportCommandHandler(
-        ITransportRepository repository
+        IUnitOfWork unitOfWork
         ) : IRequestHandler<DeleteTransportCommand, Unit>
     {
         public async Task<Unit> Handle(DeleteTransportCommand request, CancellationToken cancellationToken)
         {
-            var transport = await repository.GetByIdAsync(request.Id, cancellationToken);
+            var transport = await unitOfWork.Transports.GetByIdAsync(request.Id, cancellationToken)
+                            ?? throw new Exception("Transport not found");
 
-            if (transport is null)
-                throw new Exception("Transport not found");
+            await unitOfWork.DriverTransports.DeleteByTransportIdAsync(request.Id, cancellationToken);
+            await unitOfWork.Transports.DeleteAsync(transport, cancellationToken);
 
-            await repository.DeleteAsync(transport, cancellationToken);
             return Unit.Value;
         }
     }
