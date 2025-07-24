e6045f6a423bc36d0ae9727b507b211693f189fe
Illya <illamatviuk@gmail.com>
2025-07-24 15:18:03 +0300

    Added DriverMappingProfile.

---
+++ VehicleManagementSystem.Application/Mappings/DriverMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using VehicleManagementSystem.Application.DTOs.Driver;
+using VehicleManagementSystem.Domain.Entities;
+
+namespace VehicleManagementSystem.Application.Mappings {
+    public class DriverMappingProfile : Profile {
+        public DriverMappingProfile() {
+            CreateMap<DriverEntity, DriverDto>()
+                .ForMember(dest => dest.TeamName, opt => opt.MapFrom(src => src.Team.Name))
+                .ForMember(dest => dest.VehicleCount, opt => opt.MapFrom(src => src.Vechicles.Count));
+
+        }
+    }
+}
