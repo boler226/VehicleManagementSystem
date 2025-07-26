4571b71447ad0200b6992cea2b1b82011e7cf2cb
Illya <illamatviuk@gmail.com>
2025-07-26 17:52:21 +0300

    Added TechnicianRepository.

---
+++ VehicleManagementSystem.Domain/Interfaces/Repositories/ITechnicianRepository.cs
@@ -0,0 +1,11 @@
+﻿using VehicleManagementSystem.Domain.Entities;
+
+namespace VehicleManagementSystem.Domain.Interfaces.Repositories {
+    public interface ITechnicianRepository {
+        Task<TechnicianEntity?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+        Task<List<TechnicianEntity>?> GetAllAsync(CancellationToken cancellationToken);
+        Task AddAsync(TechnicianEntity technician, CancellationToken cancellationToken);
+        Task UpdateAsync(TechnicianEntity technician, CancellationToken cancellationToken);
+        Task DeleteAsync(TechnicianEntity technician, CancellationToken cancellationToken);
+    }
+}
