220a0318efbc2314aace0ee2369d57add9c6d195
Illya <illamatviuk@gmail.com>
2025-07-26 17:53:53 +0300

    Changed DriverDto, added short version of it.

--- VehicleManagementSystem.Application/DTOs/Driver/DriverDto.cs
+++ VehicleManagementSystem.Application/DTOs/Driver/DriverDto.cs
@@ -1,12 +1,11 @@
-﻿namespace VehicleManagementSystem.Application.DTOs.Driver {
+﻿using VehicleManagementSystem.Application.DTOs.Team;
+
+namespace VehicleManagementSystem.Application.DTOs.Driver {
     public class DriverDto {
         public Guid Id { get; set; }
         public string FullName { get; set; } = null!;
         public string LicenseNumber { get; set; } = null!;
-        public Guid TeamId { get; set; }
-
-        public string? TeamName { get; set; }
-        public int VehicleCount { get; set; }
+        public TeamShortDto Team { get; set; } = null!;
     }
 
 }
---
+++ VehicleManagementSystem.Application/DTOs/Driver/DriverShortDto.cs
@@ -0,0 +1,8 @@
+﻿namespace VehicleManagementSystem.Application.DTOs.Driver {
+    public class DriverShortDto {
+        public Guid Id { get; set; }
+        public string FullName { get; set; } = null!;
+        public string LicenseNumber { get; set; } = null!;
+
+    }
+}
