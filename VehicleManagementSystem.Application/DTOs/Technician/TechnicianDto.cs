b07aaab8e558fca57047a1dff5b311f9405b87f1
Illya <illamatviuk@gmail.com>
2025-07-26 17:54:53 +0300

    Added TechnicianDto and short version of it.

---
+++ VehicleManagementSystem.Application/DTOs/Technician/TechnicianDto.cs
@@ -0,0 +1,11 @@
+﻿using VehicleManagementSystem.Application.DTOs.Team;
+
+namespace VehicleManagementSystem.Application.DTOs.Technician {
+    public class TechnicianDto {
+        public Guid Id { get; set; }
+        public string FullName { get; set; } = null!;
+        public string Specialty { get; set; } = null!;
+        public TeamShortDto Team { get; set; } = null!;
+        // public List<RepairWorkDto>
+    }
+}
---
+++ VehicleManagementSystem.Application/DTOs/Technician/TechnicianShortDto.cs
@@ -0,0 +1,7 @@
+﻿namespace VehicleManagementSystem.Application.DTOs.Technician {
+    public class TechnicianShortDto {
+        public Guid Id { get; set; }
+        public string FullName { get; set; } = null!;
+        public string Specialty { get; set; } = null!;
+    }
+}
