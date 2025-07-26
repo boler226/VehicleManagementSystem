c19277f15df3a5cb300afea44661539358057e73
Illya <illamatviuk@gmail.com>
2025-07-26 17:54:34 +0300

    Added TeamDto and short version of it.

---
+++ VehicleManagementSystem.Application/DTOs/Team/TeamShortDto.cs
@@ -0,0 +1,6 @@
+﻿namespace VehicleManagementSystem.Application.DTOs.Team {
+    public class TeamShortDto {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
