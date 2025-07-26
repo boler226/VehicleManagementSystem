15fd785f59499e4818d0409eb9228864dd597367
Illya <illamatviuk@gmail.com>
2025-07-26 17:58:34 +0300

    Added GetAllTeamsQuery.

---
+++ VehicleManagementSystem.Application/Queries/Team/GetAll/GetAllTeamsQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using VehicleManagementSystem.Application.DTOs.Team;
+
+namespace VehicleManagementSystem.Application.Queries.Team.GetAll {
+    public record GetAllTeamsQuery : IRequest<List<TeamDto>>;
+}
---
+++ VehicleManagementSystem.Application/Queries/Team/GetAll/GetAllTeamsQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using MediatR;
+using VehicleManagementSystem.Application.DTOs.Team;
+using VehicleManagementSystem.Domain.Interfaces;
+
+namespace VehicleManagementSystem.Application.Queries.Team.GetAll {
+    public class GetAllTeamsQueryHandler(
+        IUnitOfWork unitOfWork,
+        IMapper mapper
+        ) : IRequestHandler<GetAllTeamsQuery, List<TeamDto>> {
+        public async Task<List<TeamDto>> Handle(GetAllTeamsQuery request, CancellationToken cancellationToken) {
+            var teams = await unitOfWork.Teams.GetAllAsync(cancellationToken)
+                        ?? throw new Exception("Teams does not exist");
+
+            foreach (var team in teams) {
+                team.Drivers = await unitOfWork.Drivers.GetAllByTeamIdAsync(team.Id, cancellationToken);
+            }
+
+            return mapper.Map<List<TeamDto>>(teams);
+        }
+    }
+}
