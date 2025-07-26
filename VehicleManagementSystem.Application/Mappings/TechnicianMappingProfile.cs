4c6ab382798ab23d0921c5aec084b1a7b3ec7fed
Illya <illamatviuk@gmail.com>
2025-07-26 17:55:54 +0300

    Added TechnicianMapping.

---
+++ VehicleManagementSystem.Application/Mappings/TechnicianMappingProfile.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+using VehicleManagementSystem.Application.DTOs.Technician;
+using VehicleManagementSystem.Domain.Entities;
+
+namespace VehicleManagementSystem.Application.Mappings {
+    public class TechnicianMappingProfile : Profile {
+        public TechnicianMappingProfile() {
+            CreateMap<TechnicianEntity, TechnicianShortDto>();
+        }
+    }
+}
