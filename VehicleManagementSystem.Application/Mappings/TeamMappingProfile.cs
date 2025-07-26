76503baa84502639ad6544a07d34c245dbb560c1
Illya <illamatviuk@gmail.com>
2025-07-26 17:55:41 +0300

    Added TeamMapping.

---
+++ VehicleManagementSystem.Application/Mappings/TeamMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using VehicleManagementSystem.Application.DTOs.Driver;
+using VehicleManagementSystem.Application.DTOs.Person;
+using VehicleManagementSystem.Application.DTOs.Team;
+using VehicleManagementSystem.Application.DTOs.Technician;
+using VehicleManagementSystem.Domain.Entities;
+
+namespace VehicleManagementSystem.Application.Mappings {
+    public class TeamMappingProfile : Profile {
+        public TeamMappingProfile() {
+            CreateMap<TeamEntity, TeamDto>();
+        }
+    }
+}
