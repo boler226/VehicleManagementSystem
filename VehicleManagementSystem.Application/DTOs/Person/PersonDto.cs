90db9debade69b6c2430410d6f3743e8c0ac8306
Illya <illamatviuk@gmail.com>
2025-07-26 17:54:05 +0300

    Added PersonDto.

---
+++ VehicleManagementSystem.Application/DTOs/Person/PersonDto.cs
@@ -0,0 +1,7 @@
+﻿namespace VehicleManagementSystem.Application.DTOs.Person {
+    public class PersonDto {
+        public Guid Id { get; set; }
+        public string FullName { get; set; } = null!;
+        public string Position { get; set; } = null!;
+    }
+}
