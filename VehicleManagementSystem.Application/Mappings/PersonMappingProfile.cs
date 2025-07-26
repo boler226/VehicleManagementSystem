38df5a4234d4d215ca46802b23e2306e0bb90afc
Illya <illamatviuk@gmail.com>
2025-07-26 17:55:29 +0300

    Added PersonMapping.

---
+++ VehicleManagementSystem.Application/Mappings/PersonMappingProfile.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+using VehicleManagementSystem.Application.DTOs.Person;
+using VehicleManagementSystem.Domain.Entities;
+
+namespace VehicleManagementSystem.Application.Mappings {
+    public class PersonMappingProfile : Profile {
+        public PersonMappingProfile() {
+            CreateMap<PersonEntity, PersonDto>();
+        }
+    }
+}
