ac0c49ad6c577341551b4fcc1ef5ffb89c66bc26
Illya <illamatviuk@gmail.com>
2025-07-28 21:21:35 +0300

    Added GetAllTechniciansQuery.

---
+++ VehicleManagementSystem.Application/Queries/Technician/GetAll/GetAllTechniciansQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using VehicleManagementSystem.Application.DTOs.Technician;
+
+namespace VehicleManagementSystem.Application.Queries.Technician.GetAll
+{
+    public record GetAllTechniciansQuery : IRequest<List<TechnicianDto>>;
+}
---
+++ VehicleManagementSystem.Application/Queries/Technician/GetAll/GetAllTechniciansQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using MediatR;
+using VehicleManagementSystem.Application.DTOs.Technician;
+using VehicleManagementSystem.Domain.Interfaces;
+
+namespace VehicleManagementSystem.Application.Queries.Technician.GetAll
+{
+    public class GetAllTechniciansQueryHandler(
+        IUnitOfWork unitOfWork,
+        IMapper mapper
+        ) : IRequestHandler<GetAllTechniciansQuery, List<TechnicianDto>>
+    {
+        public async Task<List<TechnicianDto>> Handle(GetAllTechniciansQuery request, CancellationToken cancellationToken)
+        {
+            var technicians = await unitOfWork.Technicians.GetAllAsync(cancellationToken)
+                             ?? throw new Exception("Technicians does not exist");
+
+            return mapper.Map<List<TechnicianDto>>(technicians);
+        }
+    }
+}
