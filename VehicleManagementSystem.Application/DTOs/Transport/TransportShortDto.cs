140f09e6df84567679be4dd3e1b83ed3d532f80f
Illya <illamatviuk@gmail.com>
2025-07-28 21:24:41 +0300

    Added TransportShortDto.

---
+++ VehicleManagementSystem.Application/DTOs/Transport/TransportShortDto.cs
@@ -0,0 +1,8 @@
+﻿namespace VehicleManagementSystem.Application.DTOs.Transport {
+    public class TransportShortDto {
+        public Guid Id { get; set; }
+        public string LicensePlate { get; set; } = null!;
+        public string Brand { get; set; } = null!;
+        public string Model { get; set; } = null!;
+    }
+}
