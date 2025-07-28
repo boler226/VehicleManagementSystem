c157b069bb4e1c18adb5b4b8283750703ea2d7d6
Illya <illamatviuk@gmail.com>
2025-07-28 21:27:30 +0300

    Added DriverTransportsController.

---
+++ VehicleManagementSystem.API/Controllers/DriverTransportsController.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using VehicleManagementSystem.Application.Commands.DriverTransport.Add;
+using VehicleManagementSystem.Application.Commands.DriverTransport.Delete;
+
+namespace VehicleManagementSystem.API.Controllers {
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class DriverTransportsController(IMediator mediator) : ControllerBase {
+        [HttpPost]
+        public async Task<IActionResult> Add(AddDriverTransportCommand command) =>
+            Ok(await mediator.Send(command));
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(DeleteDriverTransportCommand command) =>
+            Ok(await mediator.Send(command));
+    }
+}
