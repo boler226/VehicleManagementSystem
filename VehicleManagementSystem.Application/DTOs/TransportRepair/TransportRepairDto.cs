8900b03b1e20f05683b40085573c034bafb22d31
Illya <illamatviuk@gmail.com>
2025-07-30 21:53:08 +0300

    Added TransportRepair and Short Dto's.

---
+++ VehicleManagementSystem.Application/DTOs/TransportRepair/TransportRepairDto.cs
@@ -0,0 +1,12 @@
+﻿using VehicleManagementSystem.Application.DTOs.RepairWork;
+using VehicleManagementSystem.Application.DTOs.Transport;
+
+namespace VehicleManagementSystem.Application.DTOs.TransportRepair {
+    public class TransportRepairDto {
+        public Guid Id { get; set; }
+        public TransportDto Transport { get; set; } = null!;
+        public DateTime RepairDate { get; set; }
+        public double Cost { get; set; }
+        public List<RepairWorkDto> RepairWorks { get; set; } = new();
+    }
+}
---
+++ VehicleManagementSystem.Application/DTOs/TransportRepair/TransportRepairShortDto.cs
@@ -0,0 +1,9 @@
+﻿using VehicleManagementSystem.Application.DTOs.Transport;
+
+namespace VehicleManagementSystem.Application.DTOs.TransportRepair {
+    public class TransportRepairShortDto {
+        public Guid Id { get; set; }
+        public DateTime RepairDate { get; set; }
+        public double Cost { get; set; }
+    }
+}
