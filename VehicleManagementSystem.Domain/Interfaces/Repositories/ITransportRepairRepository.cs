5e6950e513647192338bcf778458de9d45ae955b
Illya <illamatviuk@gmail.com>
2025-07-30 21:51:08 +0300

    Added TransportRepairRepository.

---
+++ VehicleManagementSystem.Domain/Interfaces/Repositories/ITransportRepairRepository.cs
@@ -0,0 +1,11 @@
+﻿using VehicleManagementSystem.Domain.Entities;
+
+namespace VehicleManagementSystem.Domain.Interfaces.Repositories {
+    public interface ITransportRepairRepository {
+        Task<TransportRepairEntity?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+        Task<List<TransportRepairEntity>?> GetAllAsync(CancellationToken cancellationToken);
+        Task AddAsync(TransportRepairEntity repair, CancellationToken cancellationToken);
+        Task UpdateAsync(TransportRepairEntity repair, CancellationToken cancellationToken);
+        Task DeleteAsync(TransportRepairEntity repair, CancellationToken cancellationToken);
+    }
+}
---
+++ VehicleManagementSystem.Infrastructure/Repositories/TransportRepairRepository.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Driver;
+using VehicleManagementSystem.Domain.Entities;
+using VehicleManagementSystem.Domain.Interfaces.Repositories;
+using VehicleManagementSystem.Infrastructure.DbContext;
+
+namespace VehicleManagementSystem.Infrastructure.Repositories {
+    public class TransportRepairRepository : ITransportRepairRepository {
+        private readonly IMongoCollection<TransportRepairEntity> _collection;
+
+        public TransportRepairRepository(MongoDbContext context) {
+            _collection = context.GetCollection<TransportRepairEntity>("TransportRepairs");
+        }
+
+        public async Task<TransportRepairEntity?> GetByIdAsync(Guid id, CancellationToken cancellationToken) =>
+         await _collection.Find(r => r.Id == id).FirstOrDefaultAsync(cancellationToken);
+
+        public async Task<List<TransportRepairEntity>?> GetAllAsync(CancellationToken cancellationToken) =>
+            await _collection.Find(_ => true).ToListAsync(cancellationToken);
+
+        public async Task AddAsync(TransportRepairEntity repair, CancellationToken cancellationToken) =>
+            await _collection.InsertOneAsync(repair, cancellationToken: cancellationToken);
+
+        public async Task UpdateAsync(TransportRepairEntity repair, CancellationToken cancellationToken) =>
+            await _collection.ReplaceOneAsync(r => r.Id == repair.Id, repair, cancellationToken: cancellationToken);
+
+        public async Task DeleteAsync(TransportRepairEntity repair, CancellationToken cancellationToken) =>
+            await _collection.DeleteOneAsync(r => r.Id == repair.Id, cancellationToken: cancellationToken);
+    }
+}
