a7d7383f1f7c44f4a3cf0e288b6c322739f69db8
Illya <illamatviuk@gmail.com>
2025-07-30 21:50:53 +0300

    Added RepairWorkRepository.

---
+++ VehicleManagementSystem.Domain/Interfaces/Repositories/IRepairWorkRepository.cs
@@ -0,0 +1,12 @@
+﻿using VehicleManagementSystem.Domain.Entities;
+
+namespace VehicleManagementSystem.Domain.Interfaces.Repositories {
+    public interface IRepairWorkRepository {
+        Task<RepairWorkEntity?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+        Task<List<RepairWorkEntity>?> GetAllAsync(CancellationToken cancellationToken);
+        Task<List<RepairWorkEntity>?> GetAllByTechnicianIdAsync(Guid technicianId, CancellationToken cancellationToken);
+        Task AddAsync(RepairWorkEntity work, CancellationToken cancellationToken);
+        Task UpdateAsync(RepairWorkEntity work, CancellationToken cancellationToken);
+        Task DeleteAsync(RepairWorkEntity work, CancellationToken cancellationToken);
+    }
+}
