e3316a568a346ac7472fa9b01eb5d1a0b9ef9b37
Illya <illamatviuk@gmail.com>
2025-07-30 21:52:21 +0300

    Added RepairWork and Short Dto's.

---
+++ VehicleManagementSystem.Application/DTOs/RepairWork/RepairWorkShortDto.cs
@@ -0,0 +1,7 @@
+﻿namespace VehicleManagementSystem.Application.DTOs.RepairWork {
+    public class RepairWorkShortDto {
+        public Guid Id { get; set; }
+        public string PartName { get; set; } = null!;
+        public string WorkDescription { get; set; } = null!;
+    }
+}
