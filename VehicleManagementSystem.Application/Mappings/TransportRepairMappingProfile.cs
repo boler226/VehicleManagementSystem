a38f31b7efa7e0e223cfcd90c75f39f89a75cf0c
Illya <illamatviuk@gmail.com>
2025-07-30 21:53:54 +0300

    Added RepairWorkMapping.

---
+++ VehicleManagementSystem.Application/Mappings/TransportRepairMappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using VehicleManagementSystem.Application.DTOs.TransportRepair;
+using VehicleManagementSystem.Domain.Entities;
+
+namespace VehicleManagementSystem.Application.Mappings {
+    public class TransportRepairMappingProfile : Profile {
+        public TransportRepairMappingProfile() {
+            CreateMap<TransportRepairEntity, TransportRepairDto>();
+            CreateMap<TransportRepairEntity, TransportRepairShortDto>();
+        }
+    }
+}
