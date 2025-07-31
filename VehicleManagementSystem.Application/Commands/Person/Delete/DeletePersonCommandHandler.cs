6cd9272f564427a3f1a93279f347318d6f89cc8b
Illya <illamatviuk@gmail.com>
2025-07-31 20:49:11 +0300

    Update DeletePersonCommand, update team after delete peron.

--- VehicleManagementSystem.Application/Commands/Person/Delete/DeletePersonCommandHandler.cs
+++ VehicleManagementSystem.Application/Commands/Person/Delete/DeletePersonCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using VehicleManagementSystem.Domain.Entities;
 using VehicleManagementSystem.Domain.Interfaces;
 
 namespace VehicleManagementSystem.Application.Commands.Person.Delete {
@@ -9,8 +10,28 @@ namespace VehicleManagementSystem.Application.Commands.Person.Delete {
             var person = await unitOfWork.Persons.GetByIdAsync(request.Id, cancellationToken)
                          ?? throw new Exception("Person not found");
 
-            // realize UpdateTeam!
             await unitOfWork.Persons.DeleteAsync(person, cancellationToken);
+
+            var team = await unitOfWork.Teams.GetByPersonIdAsync(request.Id, cancellationToken);
+
+            if (team is not null) {
+
+                var roleMap = new (Func<TeamEntity, Guid?> GetId, Action<TeamEntity> Clear)[] {
+                   (t => t.ForemanId,       t => { t.ForemanId = null; t.Foreman = null; }),
+                   (t => t.MasterId,        t => { t.MasterId = null; t.Master = null; }),
+                   (t => t.SectionHeadId,   t => { t.SectionHeadId = null; t.SectionHead = null; }),
+                   (t => t.WorkshopHeadId,  t => { t.WorkshopHeadId = null; t.WorkshopHead = null; })
+                };
+
+                foreach (var (getId, clear) in roleMap) {
+                    if (getId(team) == person.Id) {
+                        clear(team);
+                        await unitOfWork.Teams.UpdateAsync(team, cancellationToken);
+                        break;
+                    }
+                }
+            }
+
             return Unit.Value;
         }
     }
