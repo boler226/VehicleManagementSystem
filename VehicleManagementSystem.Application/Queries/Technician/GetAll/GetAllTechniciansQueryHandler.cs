b3b84f92426577fbc24098931f86eadd83a9bceb
Illya <illamatviuk@gmail.com>
2025-07-31 20:47:39 +0300

    Changed GetAllTechniciansQuery.

--- VehicleManagementSystem.Application/Queries/Technician/GetAll/GetAllTechniciansQueryHandler.cs
+++ VehicleManagementSystem.Application/Queries/Technician/GetAll/GetAllTechniciansQueryHandler.cs
@@ -15,6 +15,10 @@ namespace VehicleManagementSystem.Application.Queries.Technician.GetAll
             var technicians = await unitOfWork.Technicians.GetAllAsync(cancellationToken)
                              ?? throw new Exception("Technicians does not exist");
 
+            foreach (var technician in technicians) 
+                technician.RepairWorks = await unitOfWork.RepairWorks.GetAllByTechnicianIdAsync(technician.Id, cancellationToken);
+            
+
             return mapper.Map<List<TechnicianDto>>(technicians);
         }
     }
