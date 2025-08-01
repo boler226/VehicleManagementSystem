bc4d6d3e410fbe525e4b5df53f33777eb7d2f023
Illya <illamatviuk@gmail.com>
2025-08-01 21:46:28 +0300

    Added Route dto`s.

---
+++ VehicleManagementSystem.Application/DTOs/Route/RouteDto.cs
@@ -0,0 +1,10 @@
+﻿using VehicleManagementSystem.Application.DTOs.RouteAssignment;
+
+namespace VehicleManagementSystem.Application.DTOs.Route {
+    public class RouteDto {
+        public Guid Id { get; set; }
+        public string RouterNumber { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public List<RouteAssignmentShortDto> Assignments { get; set; } = new();
+    }
+}
