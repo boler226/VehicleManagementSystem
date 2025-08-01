3cdfa3da0c425234e468f9c1b6c3512f68c1b3ae
Illya <illamatviuk@gmail.com>
2025-08-01 21:47:52 +0300

    Added RouteMappingProfile.

---
+++ VehicleManagementSystem.Application/Mappings/RouteMappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using VehicleManagementSystem.Application.DTOs.Route;
+using VehicleManagementSystem.Domain.Entities;
+
+namespace VehicleManagementSystem.Application.Mappings {
+    public class RouteMappingProfile : Profile {
+        public RouteMappingProfile() { 
+            CreateMap<RouteEntity, RouteDto>();
+            CreateMap<RouteEntity, RouteShortDto>();
+        }
+    }
+}
