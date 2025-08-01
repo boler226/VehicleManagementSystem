772c56bb1eb671507679b42d5b9a35ad2f96af45
Illya <illamatviuk@gmail.com>
2025-08-01 15:41:14 +0300

    Fixed AddTeamCommand adding all existing persons.

--- VehicleManagementSystem.Application/Commands/Team/Add/AddTeamCommandHandler.cs
+++ VehicleManagementSystem.Application/Commands/Team/Add/AddTeamCommandHandler.cs
@@ -7,6 +7,18 @@ namespace VehicleManagementSystem.Application.Commands.Team.Add {
         IUnitOfWork unitOfWork
         ) : IRequestHandler<AddTeamCommand, Guid> {
         public async Task<Guid> Handle(AddTeamCommand request, CancellationToken cancellationToken) {
+            var team = new TeamEntity {
+                Id = Guid.NewGuid(),
+                Name = request.Name,
+            };
+
+            var roleSetters = new Dictionary<string, Action<PersonEntity>> {
+                { "Foreman", p => { team.Foreman = p; team.ForemanId = p.Id; } },
+                { "Master", p => { team.Master = p; team.MasterId = p.Id; } },
+                { "SectionHead", p => { team.SectionHead = p; team.SectionHeadId = p.Id; } },
+                { "WorkshopHead", p => { team.WorkshopHead = p; team.WorkshopHeadId = p.Id; } }
+            };
+
             var personRoles = new Dictionary<string, Guid?> {
                 { "Foreman", request.ForemanId },
                 { "Master", request.MasterId },
@@ -14,21 +26,14 @@ namespace VehicleManagementSystem.Application.Commands.Team.Add {
                 { "WorkshopHead", request.WorkshopHeadId }
             };
 
-            foreach ( var role in personRoles) {
-                if (role.Value.HasValue) {
-                    var person = await unitOfWork.Persons.GetByIdAsync(role.Value.Value, cancellationToken)
-                                 ?? throw new Exception($"{role.Key} with ID {role.Value.Value} not found");
+            foreach (var (role, personId) in personRoles) {
+                if (personId.HasValue) {
+                    var person = await unitOfWork.Persons.GetByIdAsync(personId.Value, cancellationToken)
+                                 ?? throw new Exception($"{role} with ID {personId.Value} not found");
+
+                    roleSetters[role](person);
                 }
             }
-
-            var team = new TeamEntity {
-                Id = Guid.NewGuid(),
-                Name = request.Name,
-                ForemanId = request.ForemanId,
-                MasterId = request.MasterId,
-                SectionHeadId = request.SectionHeadId,
-                WorkshopHeadId = request.WorkshopHeadId
-            };
 
             await unitOfWork.Teams.AddAsync(team, cancellationToken);
 
