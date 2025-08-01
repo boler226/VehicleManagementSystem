6a3d0fcad59fa9c13af060ab0edff7919fa746e0
Illya <illamatviuk@gmail.com>
2025-08-01 15:41:49 +0300

    Added TransportRepairsController.

---
+++ VehicleManagementSystem.API/Controllers/TransportRepairsController.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using VehicleManagementSystem.Application.Commands.TransportRepair.Add;
+using VehicleManagementSystem.Application.Commands.TransportRepair.Delete;
+using VehicleManagementSystem.Application.Commands.TransportRepair.Update;
+using VehicleManagementSystem.Application.Queries.TransportRepair.GetAll;
+
+namespace VehicleManagementSystem.API.Controllers {
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class TransportRepairsController(IMediator mediator) : ControllerBase {
+        [HttpGet]
+        public async Task<IActionResult> GetAll() =>
+         Ok(await mediator.Send(new GetAllTransportRepairsQuery()));
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddTransportRepairCommand command) =>
+            Ok(await mediator.Send(command));
+
+        [HttpPut]
+        public async Task<IActionResult> Update(UpdateTransportRepairCommand command) =>
+            Ok(await mediator.Send(command));
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(DeleteTransportRepairCommand command) =>
+            Ok(await mediator.Send(command));
+    }
+}
