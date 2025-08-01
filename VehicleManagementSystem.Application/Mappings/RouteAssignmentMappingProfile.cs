2afc800c72eeb3225e15e7363c3035fe90db2272
Illya <illamatviuk@gmail.com>
2025-08-01 21:48:05 +0300

    Added RouteAssignmentMappingProfile.

---
+++ VehicleManagementSystem.Application/Mappings/RouteAssignmentMappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using VehicleManagementSystem.Application.DTOs.RouteAssignment;
+using VehicleManagementSystem.Domain.Entities;
+
+namespace VehicleManagementSystem.Application.Mappings {
+    public class RouteAssignmentMappingProfile : Profile {
+        public RouteAssignmentMappingProfile() {
+            CreateMap<RouteAssignmentEntity, RouteAssignmentDto>();
+            CreateMap<RouteAssignmentEntity, RouteAssignmentShortDto>();
+        }
+    }
+}
