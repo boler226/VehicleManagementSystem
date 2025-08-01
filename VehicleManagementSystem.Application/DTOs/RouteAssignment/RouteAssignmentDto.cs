3020e41ce2284ac961da139caecd3768b8337caf
Illya <illamatviuk@gmail.com>
2025-08-01 21:47:14 +0300

    Added RouteAssignment dto`s.

--- VehicleManagementSystem.Application/DTOs/RouteAssignment/RouteAssignmentDto.cs
+++ VehicleManagementSystem.Application/DTOs/RouteAssignment/RouteAssignmentDto.cs
@@ -1,14 +1,13 @@
-﻿namespace VehicleManagementSystem.Application.DTOs.RouteAssignment
-{
-    public class RouteAssignmentDto
-    {
-        public Guid Id { get; set; }
-        public Guid TransportId { get; set; }
-        public TransportBasicDto Transport { get; set; } = null!;
-        public Guid RouteId { get; set; }
-        public RouteDto Route { get; set; } = null!;
+﻿using VehicleManagementSystem.Application.DTOs.Route;
+using VehicleManagementSystem.Application.DTOs.Transport;
 
+namespace VehicleManagementSystem.Application.DTOs.RouteAssignment {
+    public class RouteAssignmentDto {
+        public Guid Id { get; set; }
         public DateTime Date { get; set; }
         public int PassengersCarried { get; set; }
+
+        public TransportShortDto Transport { get; set; } = null!;
+        public RouteShortDto Route { get; set; } = null!;
     }
 }
---
+++ VehicleManagementSystem.Application/DTOs/RouteAssignment/RouteAssignmentShortDto.cs
@@ -0,0 +1,7 @@
+﻿namespace VehicleManagementSystem.Application.DTOs.RouteAssignment {
+    public class RouteAssignmentShortDto {
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; }
+        public int PassengersCarried { get; set; }
+    }
+}
