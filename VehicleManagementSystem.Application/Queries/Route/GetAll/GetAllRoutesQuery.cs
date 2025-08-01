af6047c87b819c43b42947fb50533ba4ef2d43b6
Illya <illamatviuk@gmail.com>
2025-08-01 21:48:25 +0300

    Added GetAllRoutesQuery.

---
+++ VehicleManagementSystem.Application/Queries/Route/GetAll/GetAllRoutesQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using VehicleManagementSystem.Application.DTOs.Route;
+
+namespace VehicleManagementSystem.Application.Queries.Route.GetAll {
+    public record GetAllRoutesQuery : IRequest<List<RouteDto>>;
+}
---
+++ VehicleManagementSystem.Application/Queries/Route/GetAll/GetAllRoutesQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MediatR;
+using VehicleManagementSystem.Application.DTOs.Route;
+using VehicleManagementSystem.Domain.Interfaces;
+
+namespace VehicleManagementSystem.Application.Queries.Route.GetAll {
+    public class GetAllRoutesQueryHandler(
+        IUnitOfWork unitOfWork,
+        IMapper mapper
+        ) : IRequestHandler<GetAllRoutesQuery, List<RouteDto>> {
+        public async Task<List<RouteDto>> Handle(GetAllRoutesQuery request, CancellationToken cancellationToken) {
+            var routes = await unitOfWork.Routes.GetAllAsync(cancellationToken)
+                         ?? throw new Exception("Routes does not exist");
+
+            foreach (var route in routes) {
+                var assignments = await unitOfWork.RouteAssignments.GetAllByRouteIdAsync(route.Id, cancellationToken);
+
+                if (assignments is not null)
+                    route.Assignments = assignments.ToList();
+            }
+
+            return mapper.Map<List<RouteDto>>(routes);
+        }
+    }
+}
