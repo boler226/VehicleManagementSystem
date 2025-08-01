922626006e2e0b78c509be9dda62c0c828735e72
Illya <illamatviuk@gmail.com>
2025-08-01 21:51:16 +0300

    Added RoutesController.

---
+++ VehicleManagementSystem.API/Controllers/RoutesController.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using VehicleManagementSystem.Application.Commands.Route.Add;
+using VehicleManagementSystem.Application.Commands.Route.Delete;
+using VehicleManagementSystem.Application.Commands.Route.Update;
+using VehicleManagementSystem.Application.Queries.Route.GetAll;
+
+namespace VehicleManagementSystem.API.Controllers {
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class RoutesController(IMediator mediator) : ControllerBase {
+        [HttpGet]
+        public async Task<IActionResult> GetAll() =>
+          Ok(await mediator.Send(new GetAllRoutesQuery()));
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddRouteCommand command) =>
+            Ok(await mediator.Send(command));
+
+        [HttpPut]
+        public async Task<IActionResult> Update(UpdateRouteCommand command) =>
+            Ok(await mediator.Send(command));
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(DeleteRouteCommand command) =>
+            Ok(await mediator.Send(command));
+    }
+}
