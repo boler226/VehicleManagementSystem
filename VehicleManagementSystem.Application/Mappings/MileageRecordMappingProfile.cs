f33d5e4fae37b8a1950d58d07107afc3db7182b0
Illya <illamatviuk@gmail.com>
2025-08-27 11:31:18 +0300

    Added MileageRecordMappingProfile.

---
+++ VehicleManagementSystem.Application/Mappings/MileageRecordMappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using VehicleManagementSystem.Application.DTOs.MileageRecord;
+using VehicleManagementSystem.Domain.Entities;
+
+namespace VehicleManagementSystem.Application.Mappings {
+    public class MileageRecordMappingProfile : Profile {
+        public MileageRecordMappingProfile() {
+            CreateMap<MileageRecordEntity, MileageRecordDto>();
+            CreateMap<MileageRecordEntity, MileageRecordShortDto>();
+        }
+    }
+}
