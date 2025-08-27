32b465de212e755819d6c00d869e21842c60c2e0
Illya <illamatviuk@gmail.com>
2025-08-27 16:50:50 +0300

    Added GarageObject and Short Dto`s.

---
+++ VehicleManagementSystem.Application/DTOs/GarageObject/GarageObjectDto.cs
@@ -0,0 +1,10 @@
+﻿using VehicleManagementSystem.Application.DTOs.Transport;
+
+namespace VehicleManagementSystem.Application.DTOs.GarageObject {
+    public class GarageObjectDto {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Location { get; set; } = null!;
+        public List<TransportShortDto> VehiclesStored { get; set; } = new();
+    }
+}
---
+++ VehicleManagementSystem.Application/DTOs/GarageObject/GarageObjectShortDto.cs
@@ -0,0 +1,7 @@
+﻿namespace VehicleManagementSystem.Application.DTOs.GarageObject {
+    public class GarageObjectShortDto {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Location { get; set; } = null!;
+    }
+}
