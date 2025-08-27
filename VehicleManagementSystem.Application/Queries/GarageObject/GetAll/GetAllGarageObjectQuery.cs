71e98486f64844d657e5b558400265fe12ffd4aa
Illya <illamatviuk@gmail.com>
2025-08-27 16:52:02 +0300

    Added GetAllGarageObjectQuery.

---
+++ VehicleManagementSystem.Application/Queries/GarageObject/GetAll/GetAllGarageObjectQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using VehicleManagementSystem.Application.DTOs.GarageObject;
+
+namespace VehicleManagementSystem.Application.Queries.GarageObject.GetAll {
+    public record GetAllGarageObjectQuery : IRequest<List<GarageObjectDto>>;
+}
---
+++ VehicleManagementSystem.Application/Queries/GarageObject/GetAll/GetAllGarageObjectQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MediatR;
+using VehicleManagementSystem.Application.DTOs.GarageObject;
+using VehicleManagementSystem.Domain.Interfaces;
+
+namespace VehicleManagementSystem.Application.Queries.GarageObject.GetAll {
+    public class GetAllGarageObjectQueryHandler(
+        IUnitOfWork unitOfWork,
+        IMapper mapper
+        ) : IRequestHandler<GetAllGarageObjectQuery, List<GarageObjectDto>> {
+        public async Task<List<GarageObjectDto>> Handle(GetAllGarageObjectQuery request, CancellationToken cancellationToken) {
+            var garageObjects = await unitOfWork.GarageObjects.GetAllAsync(cancellationToken);
+
+            if (garageObjects is not null) {
+                foreach (var garage in garageObjects) {
+                    var transports = await unitOfWork.Transports.GetAllByGarageIdAsync(garage.Id, cancellationToken);
+
+                    if (transports is not null) 
+                        garage.VehiclesStored = transports;
+                }
+            }
+
+            return mapper.Map<List<GarageObjectDto>>(garageObjects);
+        }
+    }
+}
