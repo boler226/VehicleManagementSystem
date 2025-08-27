d7527a8bb48da6030f4b8638b6dac905283bf26e
Illya <illamatviuk@gmail.com>
2025-08-27 16:54:40 +0300

    Updated UpdateTransportCommand, added GarageObject.

--- VehicleManagementSystem.Application/Commands/Transport/Update/UpdateTransportCommand.cs
+++ VehicleManagementSystem.Application/Commands/Transport/Update/UpdateTransportCommand.cs
@@ -2,5 +2,5 @@
 using VehicleManagementSystem.Domain.Enums;
 
 namespace VehicleManagementSystem.Application.Commands.Transport.Update {
-    public record UpdateTransportCommand(Guid Id, string? LicensePlate, TransportEnum? Type, int? Capacity, double? LoadCapacity) : IRequest<Unit>;
+    public record UpdateTransportCommand(Guid Id, Guid? GarageId, string? LicensePlate, TransportEnum? Type, int? Capacity, double? LoadCapacity) : IRequest<Unit>;
 }
--- VehicleManagementSystem.Application/Commands/Transport/Update/UpdateTransportCommandHandler.cs
+++ VehicleManagementSystem.Application/Commands/Transport/Update/UpdateTransportCommandHandler.cs
@@ -7,7 +7,15 @@ namespace VehicleManagementSystem.Application.Commands.Transport.Update {
         ) : IRequestHandler<UpdateTransportCommand, Unit> {
         public async Task<Unit> Handle(UpdateTransportCommand request, CancellationToken cancellationToken) {
             var transport = await unitOfWork.Transports.GetByIdAsync(request.Id, cancellationToken)
-                ?? throw new Exception("Transport not found");
+                            ?? throw new Exception("Transport not found");
+
+            if (request.GarageId.HasValue) {
+                var garage = await unitOfWork.GarageObjects.GetByIdAsync(request.GarageId.Value, cancellationToken)
+                             ?? throw new Exception("Garage object not found");
+
+                transport.GarageObject = garage;
+                transport.GarageObjectId = garage.Id;
+            }
 
             if (!string.IsNullOrWhiteSpace(request.LicensePlate))
                 transport.LicensePlate = request.LicensePlate;
