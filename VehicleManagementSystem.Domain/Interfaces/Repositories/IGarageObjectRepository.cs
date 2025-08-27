79e11f581565b3fe8e4bc5ff711f9b9a22889bce
Illya <illamatviuk@gmail.com>
2025-08-27 16:48:26 +0300

    Added GarageObjectRepository.

---
+++ VehicleManagementSystem.Domain/Interfaces/Repositories/IGarageObjectRepository.cs
@@ -0,0 +1,11 @@
+﻿using VehicleManagementSystem.Domain.Entities;
+
+namespace VehicleManagementSystem.Domain.Interfaces.Repositories {
+    public interface IGarageObjectRepository {
+        Task<GarageObjectEntity?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+        Task<List<GarageObjectEntity>?> GetAllAsync(CancellationToken cancellationToken);
+        Task AddAsync(GarageObjectEntity garage, CancellationToken cancellationToken);
+        Task UpdateAsync(GarageObjectEntity garage, CancellationToken cancellationToken);
+        Task DeleteAsync(GarageObjectEntity garage, CancellationToken cancellationToken);
+    }
+}
