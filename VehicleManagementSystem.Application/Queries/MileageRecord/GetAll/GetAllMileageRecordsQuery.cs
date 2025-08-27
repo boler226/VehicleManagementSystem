f38ba921828b0b78c93d11d64328b9e102ad630d
Illya <illamatviuk@gmail.com>
2025-08-27 11:32:20 +0300

    Added GetAllMileageRecordsQuery.

---
+++ VehicleManagementSystem.Application/Queries/MileageRecord/GetAll/GetAllMileageRecordsQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using VehicleManagementSystem.Application.DTOs.MileageRecord;
+
+namespace VehicleManagementSystem.Application.Queries.MileageRecord.GetAll {
+    public record GetAllMileageRecordsQuery() : IRequest<List<MileageRecordDto>>;
+}
---
+++ VehicleManagementSystem.Application/Queries/MileageRecord/GetAll/GetAllMileageRecordsQueryHandler.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using MediatR;
+using VehicleManagementSystem.Application.DTOs.MileageRecord;
+using VehicleManagementSystem.Domain.Interfaces;
+
+namespace VehicleManagementSystem.Application.Queries.MileageRecord.GetAll {
+    public class GetAllMileageRecordsQueryHandler(
+        IUnitOfWork unitOfWork,
+        IMapper mapper
+        ) : IRequestHandler<GetAllMileageRecordsQuery, List<MileageRecordDto>> {
+        public async Task<List<MileageRecordDto>> Handle(GetAllMileageRecordsQuery request, CancellationToken cancellationToken) {
+            var mileageRecords = await unitOfWork.MileageRecords.GetAllAsync(cancellationToken)
+                                 ?? throw new Exception("MileageRecords does not exist");
+
+            return mapper.Map<List<MileageRecordDto>>(mileageRecords);
+        }
+    }
+}
