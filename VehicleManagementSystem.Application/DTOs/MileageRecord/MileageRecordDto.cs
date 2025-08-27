46dd182285d989f668d198df4a9b664e43b5851e
Illya <illamatviuk@gmail.com>
2025-08-27 11:30:25 +0300

    Added MileageRecordDto and ShortDto.

---
+++ VehicleManagementSystem.Application/DTOs/MileageRecord/MileageRecordDto.cs
@@ -0,0 +1,10 @@
+﻿using VehicleManagementSystem.Application.DTOs.Transport;
+
+namespace VehicleManagementSystem.Application.DTOs.MileageRecord {
+    public class MileageRecordDto {
+        public Guid Id { get; set; }
+        public TransportShortDto Transport { get; set; } = new();
+        public DateTime Date { get; set; }
+        public double Kilometers { get; set; }
+    }
+}
---
+++ VehicleManagementSystem.Application/DTOs/MileageRecord/MileageRecordShortDto.cs
@@ -0,0 +1,7 @@
+﻿namespace VehicleManagementSystem.Application.DTOs.MileageRecord {
+    public class MileageRecordShortDto {
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Kilometers { get; set; }
+    }
+}
