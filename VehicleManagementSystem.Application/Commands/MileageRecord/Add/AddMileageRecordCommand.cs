c2d97a18cec3edefdcc1f8554af4cc353f353148
Illya <illamatviuk@gmail.com>
2025-08-27 11:32:36 +0300

    Added AddMileageRecordCommand.

---
+++ VehicleManagementSystem.Application/Commands/MileageRecord/Add/AddMileageRecordCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace VehicleManagementSystem.Application.Commands.MileageRecord.Add {
+    public record AddMileageRecordCommand(DateTime Date, double Kilometers, Guid TransportId) : IRequest<Guid>;
+}
---
+++ VehicleManagementSystem.Application/Commands/MileageRecord/Add/AddMileageRecordCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using VehicleManagementSystem.Domain.Entities;
+using VehicleManagementSystem.Domain.Interfaces;
+
+namespace VehicleManagementSystem.Application.Commands.MileageRecord.Add {
+    public class AddMileageRecordCommandHandler(
+        IUnitOfWork unitOfWork
+        ) : IRequestHandler<AddMileageRecordCommand, Guid> {
+        public async Task<Guid> Handle(AddMileageRecordCommand reqeust, CancellationToken cancellationToken) {
+            var transport = await unitOfWork.Transports.GetByIdAsync(reqeust.TransportId, cancellationToken)
+                            ?? throw new Exception("Transport not found");
+
+            var mileageRecord = new MileageRecordEntity {
+                Id = Guid.NewGuid(),
+                Transport = transport,
+                TransportId = transport.Id,
+                Date = reqeust.Date,
+                Kilometers = reqeust.Kilometers
+            };
+
+            await unitOfWork.MileageRecords.AddAsync(mileageRecord, cancellationToken);
+
+            return mileageRecord.Id;
+        }
+    }
+}
