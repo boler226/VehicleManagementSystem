d6bbcb3ba08cd67ad09bfed9d8e5429984f2423b
Illya <illamatviuk@gmail.com>
2025-08-27 11:29:45 +0300

    Added MileageRecordRepository.

---
+++ VehicleManagementSystem.Domain/Interfaces/Repositories/IMileageRecordRepository.cs
@@ -0,0 +1,12 @@
+﻿using VehicleManagementSystem.Domain.Entities;
+
+namespace VehicleManagementSystem.Domain.Interfaces.Repositories {
+    public interface IMileageRecordRepository {
+        Task<MileageRecordEntity?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+        Task<List<MileageRecordEntity>?> GetAllAsync(CancellationToken cancellationToken);
+        Task<List<MileageRecordEntity>?> GetAllByTransportIdAsync(Guid transportId, CancellationToken cancellationToken);
+        Task AddAsync(MileageRecordEntity mileageRecord, CancellationToken cancellationToken);
+        Task UpdateAsync(MileageRecordEntity mileageRecord, CancellationToken cancellationToken);
+        Task DeleteAsync(MileageRecordEntity mileageRecord, CancellationToken cancellationToken);
+    }
+}
