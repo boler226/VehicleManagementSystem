d0c2d73531dc249fa270a0f4ac3cd4ecde5b3890
Illya <illamatviuk@gmail.com>
2025-11-15 15:54:06 +0200

    Changed MongoDbContext.

--- VehicleManagementSystem.Infrastructure/DbContext/MongoDbContext.cs
+++ VehicleManagementSystem.Infrastructure/DbContext/MongoDbContext.cs
@@ -1,30 +1,34 @@
 ﻿using Microsoft.Extensions.Configuration;
 using MongoDB.Driver;
 using VehicleManagementSystem.Domain.Entities;
+using VehicleManagementSystem.Domain.Entities.Identity;
 
-namespace VehicleManagementSystem.Infrastructure.DbContext {
-    public class MongoDbContext {
-        private readonly IMongoDatabase _database;
+namespace VehicleManagementSystem.Infrastructure.DbContext; 
+public class MongoDbContext {
+    private readonly IMongoDatabase _database;
 
-        public MongoDbContext(IConfiguration configuration) {
-            var client = new MongoClient(configuration.GetConnectionString("MongoDb"));
-            _database = client.GetDatabase("VehicleManagement");
-        }
-
-        public IMongoCollection<T> GetCollection<T>(string collectionName) =>
-            _database.GetCollection<T>(collectionName);
-
-        public IMongoCollection<TransportEntity> Transports => _database.GetCollection<TransportEntity>("Transports");
-        public IMongoCollection<DriverEntity> Drivers => _database.GetCollection<DriverEntity>("Drivers");
-        public IMongoCollection<DriverTransportEntity> DriverTransports => _database.GetCollection<DriverTransportEntity>("DriverTransports");
-        public IMongoCollection<TechnicianEntity> Technicians => _database.GetCollection<TechnicianEntity>("Technicians");
-        public IMongoCollection<TeamEntity> Teams => _database.GetCollection<TeamEntity>("Teams");
-        public IMongoCollection<PersonEntity> Person => _database.GetCollection<PersonEntity>("Persons");
-        public IMongoCollection<GarageObjectEntity> GarageObjects => _database.GetCollection<GarageObjectEntity>("GarageObjects");
-        public IMongoCollection<RouteEntity> Routes => _database.GetCollection<RouteEntity>("Routes");
-        public IMongoCollection<RouteAssignmentEntity> RouteAssignments => _database.GetCollection<RouteAssignmentEntity>("RouteAssignments");
-        public IMongoCollection<TransportRepairEntity> TransportRepairs => _database.GetCollection<TransportRepairEntity>("TransportRepairs");
-        public IMongoCollection<RepairWorkEntity> RepairWorks => _database.GetCollection<RepairWorkEntity>("RepairWorks");
-        public IMongoCollection<MileageRecordEntity> Mileages => _database.GetCollection<MileageRecordEntity>("Mileages");
+    public MongoDbContext(IConfiguration configuration) {
+        var client = new MongoClient(configuration.GetConnectionString("MongoDb"));
+        _database = client.GetDatabase("VehicleManagement");
     }
+
+    public IMongoCollection<T> GetCollection<T>(string collectionName)
+    {
+        return _database.GetCollection<T>(collectionName);
+    }
+
+    public IMongoCollection<TransportEntity> Transports => _database.GetCollection<TransportEntity>("Transports");
+    public IMongoCollection<DriverEntity> Drivers => _database.GetCollection<DriverEntity>("Drivers");
+    public IMongoCollection<DriverTransportEntity> DriverTransports => _database.GetCollection<DriverTransportEntity>("DriverTransports");
+    public IMongoCollection<TechnicianEntity> Technicians => _database.GetCollection<TechnicianEntity>("Technicians");
+    public IMongoCollection<TeamEntity> Teams => _database.GetCollection<TeamEntity>("Teams");
+    public IMongoCollection<PersonEntity> Person => _database.GetCollection<PersonEntity>("Persons");
+    public IMongoCollection<GarageObjectEntity> GarageObjects => _database.GetCollection<GarageObjectEntity>("GarageObjects");
+    public IMongoCollection<RouteEntity> Routes => _database.GetCollection<RouteEntity>("Routes");
+    public IMongoCollection<RouteAssignmentEntity> RouteAssignments => _database.GetCollection<RouteAssignmentEntity>("RouteAssignments");
+    public IMongoCollection<TransportRepairEntity> TransportRepairs => _database.GetCollection<TransportRepairEntity>("TransportRepairs");
+    public IMongoCollection<RepairWorkEntity> RepairWorks => _database.GetCollection<RepairWorkEntity>("RepairWorks");
+    public IMongoCollection<MileageRecordEntity> Mileages => _database.GetCollection<MileageRecordEntity>("Mileages");
+    public IMongoCollection<UserEntity> Users => _database.GetCollection<UserEntity>("Users");
+    public IMongoCollection<RoleEntity> Roles => _database.GetCollection<RoleEntity>("Roles");
 }
