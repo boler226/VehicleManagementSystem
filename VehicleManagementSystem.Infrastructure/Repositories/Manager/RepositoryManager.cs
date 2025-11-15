12e875b55f30388605a2497e88a3ea49c5f5d5ca
Illya <illamatviuk@gmail.com>
2025-11-15 15:54:26 +0200

    Added RepositoryManager.

---
+++ VehicleManagementSystem.Infrastructure/Repositories/Manager/RepositoryManager.cs
@@ -0,0 +1,33 @@
+﻿using VehicleManagementSystem.Domain.Interfaces;
+using VehicleManagementSystem.Domain.Interfaces.Repositories;
+
+namespace VehicleManagementSystem.Infrastructure.Manager;
+
+public class RepositoryManager(
+    IDriverRepository driverRepository,
+    ITransportRepository transportRepository,
+    ITeamRepository teamRepository,
+    IDriverTransportRepository driverTransportRepository,
+    IPersonRepository personRepository,
+    ITechnicianRepository technicianRepository,
+    ITransportRepairRepository repairRepository,
+    IRepairWorkRepository repairWorkRepository,
+    IRouteRepository routeRepository,
+    IRouteAssignmentRepository assignmentRepository,
+    IMileageRecordRepository recordRepository,
+    IGarageObjectRepository objectRepository
+    ) : IRepositoryManager 
+{
+    public IDriverRepository Drivers => driverRepository;
+    public ITransportRepository Transports => transportRepository;
+    public ITeamRepository Teams => teamRepository;
+    public IDriverTransportRepository DriverTransports => driverTransportRepository;
+    public IPersonRepository Persons => personRepository;
+    public ITechnicianRepository Technicians => technicianRepository;
+    public ITransportRepairRepository TransportRepairs => repairRepository;
+    public IRepairWorkRepository RepairWorks => repairWorkRepository;
+    public IRouteRepository Routes => routeRepository;
+    public IRouteAssignmentRepository RouteAssignments => assignmentRepository;
+    public IMileageRecordRepository MileageRecords => recordRepository;
+    public IGarageObjectRepository GarageObjects => objectRepository;
+}
