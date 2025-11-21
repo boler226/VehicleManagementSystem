9d1dd42a8c3e50cb96886e619c7147929939dae8
Illya <illamatviuk@gmail.com>
2025-11-21 17:19:43 +0200

    Added GetTransportRepairStatsQuery.

---
+++ VehicleManagementSystem.Application/Queries/TransportRepair/GetStats/GetTransportRepairStatsHandler.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using MediatR;
+using VehicleManagementSystem.Application.DTOs.TransportRepair;
+using VehicleManagementSystem.Domain.Interfaces;
+
+namespace VehicleManagementSystem.Application.Queries.TransportRepair.GetStats;
+public class GetTransportRepairStatsHandler(
+    IRepositoryManager repositoryManager,
+    IMapper mapper
+    ) : IRequestHandler<GetTransportRepairStatsQuery, IEnumerable<TransportRepairStatsDto>>
+{
+    public async Task<IEnumerable<TransportRepairStatsDto>> Handle(
+       GetTransportRepairStatsQuery request,
+       CancellationToken cancellationToken)
+    {
+        var transports = await repositoryManager.Transports.GetTransportsWithRepairsAsync(
+            request.Category, request.Brand, request.TransportId, request.FromDate, request.ToDate, cancellationToken);
+
+        return mapper.Map<IEnumerable<TransportRepairStatsDto>>(transports);
+    }
+}
---
+++ VehicleManagementSystem.Application/Queries/TransportRepair/GetStats/GetTransportRepairStatsQuery.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using VehicleManagementSystem.Application.DTOs.TransportRepair;
+using VehicleManagementSystem.Domain.Enums;
+
+namespace VehicleManagementSystem.Application.Queries.TransportRepair.GetStats;
+public record GetTransportRepairStatsQuery(
+    TransportEnum? Category,
+    string? Brand,
+    Guid? TransportId,
+    DateTime? FromDate,
+    DateTime? ToDate
+) : IRequest<IEnumerable<TransportRepairStatsDto>>;
