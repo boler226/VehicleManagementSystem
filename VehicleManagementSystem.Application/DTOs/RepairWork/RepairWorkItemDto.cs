90b7f887575d60ae231748dc459635a2f6cda4e7
Illya <illamatviuk@gmail.com>
2025-11-21 17:10:02 +0200

    Added RepairWorkItem and TechnicianWorksReport Dto's.

---
+++ VehicleManagementSystem.Application/DTOs/RepairWork/RepairWorkItemDto.cs
@@ -0,0 +1,11 @@
+﻿namespace VehicleManagementSystem.Application.DTOs.RepairWork;
+public class RepairWorkItemDto
+{
+    public Guid RepairId { get; set; }
+    public DateTime RepairDate { get; set; }
+    public string PartName { get; set; } = null!;
+    public string WorkDescription { get; set; } = null!;
+    public Guid TransportId { get; set; }
+    public string LicensePlate { get; set; } = null!;
+    public double Cost { get; set; }
+}
---
+++ VehicleManagementSystem.Application/DTOs/Technician/TechnicianWorksReportDto.cs
@@ -0,0 +1,15 @@
+﻿using VehicleManagementSystem.Application.DTOs.RepairWork;
+
+namespace VehicleManagementSystem.Application.DTOs.Technician;
+public class TechnicianWorksReportDto
+{
+    public Guid TechnicianId { get; set; }
+    public string FullName { get; set; } = null!;
+    public string Specialty { get; set; } = null!;
+    public int TotalWorks { get; set; }
+    public double TotalCost { get; set; }
+    public IEnumerable<RepairWorkItemDto> Works { get; set; } = new List<RepairWorkItemDto>();
+
+    public int VehicleWorksCount { get; set; }
+    public double VehicleWorksCost { get; set; }
+}
