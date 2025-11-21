e20ab9f6a55047b14aa8ccebab9e4acbc9b9313e
Illya <illamatviuk@gmail.com>
2025-11-21 17:20:28 +0200

    Added GetPassengerDistributionByRoutesQuery.

---
+++ VehicleManagementSystem.Application/Queries/Route/GetPassengerByRoutes/GetPassengerDistributionByRoutesQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using VehicleManagementSystem.Application.DTOs.RouteAssignment;
+
+namespace VehicleManagementSystem.Application.Queries.Route.GetByRoutes;
+public record GetPassengerDistributionByRoutesQuery(DateTime? FromDate, DateTime? ToDate)
+    : IRequest<IEnumerable<RoutePassengerDistributionDto>>;
---
+++ VehicleManagementSystem.Application/Queries/Route/GetPassengerByRoutes/GetPassengerDistributionByRoutesQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using VehicleManagementSystem.Application.DTOs.RouteAssignment;
+using VehicleManagementSystem.Domain.Interfaces;
+
+namespace VehicleManagementSystem.Application.Queries.Route.GetByRoutes;
+public class GetPassengerDistributionByRoutesQueryHandler(
+    IRepositoryManager repositoryManager
+    ) : IRequestHandler<GetPassengerDistributionByRoutesQuery, IEnumerable<RoutePassengerDistributionDto>>
+{
+    public async Task<IEnumerable<RoutePassengerDistributionDto>> Handle(
+        GetPassengerDistributionByRoutesQuery request,
+        CancellationToken cancellationToken)
+    {
+        var assignments = await repositoryManager.RouteAssignments.GetAssignmentsAsync(request.FromDate, request.ToDate, cancellationToken);
+
+        var grouped = assignments
+            .GroupBy(x => x.RouteId)
+            .Select(g =>
+            {
+                var route = repositoryManager.Routes.GetByIdAsync(g.Key, cancellationToken).Result;
+
+                return new RoutePassengerDistributionDto
+                {
+                    RouteId = g.Key,
+                    RouteNumber = route?.RouterNumber ?? "Unknown",
+                    Description = route?.Description ?? string.Empty,
+                    TotalPassengers = g.Sum(x => x.PassengersCarried)
+                };
+            });
+
+        return grouped;
+    }
+}
