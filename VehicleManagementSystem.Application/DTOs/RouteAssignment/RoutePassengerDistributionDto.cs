91050047a58643ddf5874875bf5effee67fc80e7
Illya <illamatviuk@gmail.com>
2025-11-21 17:14:01 +0200

    Added RoutePassengerDistributionDto.

---
+++ VehicleManagementSystem.Application/DTOs/RouteAssignment/RoutePassengerDistributionDto.cs
@@ -0,0 +1,8 @@
+﻿namespace VehicleManagementSystem.Application.DTOs.RouteAssignment;
+public class RoutePassengerDistributionDto
+{
+    public Guid RouteId { get; set; }
+    public string RouteNumber { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public int TotalPassengers { get; set; }
+}
